3681c32aa088cbbae8ab09bd1b72f8530d96d889
Катя <@DESKTOP-8CVJE59>
2024-11-21 18:27:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        double result1 = Calculate(5, 7);
+        double result2 = Calculate(12, 15);
+        double result3 = Calculate(22, 32);
+        double totalResult = result1 + result2 + result3;
+
+        Console.WriteLine($"Результат 1: {result1}");
+        Console.WriteLine($"Результат 2: {result2}");
+        Console.WriteLine($"Результат 3: {result3}");
+        Console.WriteLine($"Загальна сума: {totalResult}");
+
+         double Calculate(double x, double y)
+        {
+            return (Math.Sqrt(x) + x) / (y + Math.Sqrt(y));
+        }
+        Console.ReadKey();
+    }
+}
+
